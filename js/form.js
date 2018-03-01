2d8f7ec3ab72acc1de67adc6149db1f92a89fa66
Stoutov <anton.tikhonovlll@yandex.ru>
2018-03-01 22:01:03 +0300

    //

--- js/form.js
+++ js/form.js
@@ -21,20 +21,6 @@
   var bungaloPrice = 1000;
   var housePrice = 5000;
   var palacePrice = 10000;
-  // var inValidTypeMessage = 'Для данного типа жилья цена не может быть ниже ';
-
-  // inputs.validity = {
-  //   valid: false, // Поле валидно
-  //   customError: false, // Установленно специальное сообщение ошибки
-  //   patternMismatch: false, // Значение не удовлетворяет шаблону, установленному в атрибуте pattern
-  //   rangeOverflow: false, // Значение превосходит атрибут max
-  //   rangeUnderflow: true, // Значение меньше атрибута min
-  //   stepMismatch: true, // Значение не соответствует указаному шагу
-  //   tooLong: false, // Значение слишком длинное
-  //   tooShort: false, // Значение слишком короткое
-  //   typeMismatch: false, // Значение не соответствует указаному атрибуту type
-  //   valueMissing: false // Отсутствует обязательное значение
-  // };
 
 
   var getValidTitle = function () {
